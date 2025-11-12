d29bcde7eb676f4d1dd5749a937930b3a2cb281c
IbragimKhaniev <newinterny@gmail.com>
2025-11-13 01:28:15 +0300

    Configure Django admin for Member, Ad, ChatThread, and Message models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Member, Ad, ChatThread, Message
 
-# Register your models here.
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "name", "date_joined")
+    search_fields = ("email", "name")
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "category", "price", "author", "created_at")
+    list_filter = ("category", "condition")
+    search_fields = ("title",)
+
+
+@admin.register(ChatThread)
+class ChatThreadAdmin(admin.ModelAdmin):
+    list_display = ("id", "user1", "user2", "ad", "created_at")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "thread", "sender", "created_at", "is_read")
